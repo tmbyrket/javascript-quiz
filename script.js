4f78c864aff734bf0e6a4c85b283ad4eaf4a4170
Reese B <theresa.byrket@gmail.com>
2021-05-16 21:07:48 -0500

    write out js pseudocode

---
+++ script.js
@@ -0,0 +1,43 @@
+//define all HTML elements that will be called
+
+
+//make quiz questions an object
+
+
+//define other global variabels
+
+
+//function to go through the array of quiz questions for q's and a's
+
+
+//function for start quiz
+    //start the time range
+    //hides the start button
+    //displays the first question
+
+
+
+//function to show score after finishing quiz or running out of time
+
+
+//submit button, should be function highscore, turn into string  (the array of high schores should be save in local storage)
+//and push the new user name and score in the array for local storage
+
+
+
+//clear list for the high scores and generate a new high schore list from local
+
+
+//function to display high scores and hide all the other pages
+
+
+//function to clear local storage
+
+
+//function sets everything back to original values and goes back to homepage
+
+
+//function to check the answer
+
+
+//starts da quiz
